a34305dd3d309319c0fd1ec8c688364366f661f2
Pushpendra Kumar <pkverma2972@gmail.com>
2026-04-08 03:27:32 +0530

    updated file

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,18 @@
 
 
-
+import Chai from "./chai"
 
 function App() {
   
   return (
-   <h1> chai aur react | Pushpendra Kumar </h1>
+   <>
+   
+   <Chai/>
+   <h3> hellow react </h3>
+   
+   </>
+   
+   
   )
 }
 
