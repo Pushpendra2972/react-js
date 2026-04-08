4b57ae9b6f1d404ab26ad1ee982f658580be442d
Pushpendra Kumar <pkverma2972@gmail.com>
2026-04-08 15:57:27 +0530

    updated file new

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -11,8 +11,7 @@ function App() {
    <h3> hellow react </h3>
    
    </>
-   
-   
+
   )
 }
 
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,43 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
 
 import App from './App.jsx'
 
+
+// const myobj = (
+   
+//     <a href="https://google.com" target="_blank"> visit google </a>
+// )
+
+
+// const myElement  = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'click me to visit google'
+// }
+
+
+
+const reactElement = React.createElement(
+  'a',
+  { href: "https://google.com", target: "_blank" },
+  "click me to visit google"
+)
+
+
 createRoot(document.getElementById('root')).render(
   
-    <App />
+    // <App/>
+    // App()
+
+    // myobj
+
+    // myElement  //  won't run bcoz render have some predefined syntax 
+
+    reactElement
   
 )
